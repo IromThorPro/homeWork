02297b9e5eea4b12d5104915dfd569f8deda2aea
IromThorPro <154530849+IromThorPro@users.noreply.github.com>
2024-05-17 13:55:55 +0300

    ServerChat.py

--- ServerChat.py
+++ ServerChat.py
@@ -1,13 +1,24 @@
 import socket
 import threading
+import sqlite3
 
-def handle_client(client_socket):
+def save_message(client_address, message):
+    conn = sqlite3.connect('messages.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO messages (client_address, message) VALUES (?, ?)
+    ''', (client_address, message))
+    conn.commit()
+    conn.close()
+
+def handle_client(client_socket, client_address):
     while True:
         message = client_socket.recv(1024).decode()
         if not message:
             break
         print("Client:", message)
         client_socket.sendall(message.encode())
+        save_message(client_address, message)
     client_socket.close()
 
 def main():
@@ -20,7 +31,7 @@ def main():
     while True:
         client_socket, client_address = server_socket.accept()
         print("Connection established with", client_address)
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address[0]))
         client_thread.start()
 
 if __name__ == "__main__":
